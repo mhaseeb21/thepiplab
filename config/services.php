3a8249ab000b4ce1359cee66d026626abfd35184
Muhammad Haseeb <haseeb@Muhammads-MacBook-Air.local>
2026-02-06 21:34:03 +0500

    services.php updated

--- config/services.php
+++ config/services.php
@@ -34,5 +34,9 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
-
+    // Cloudflare Turnstile
+    'turnstile' => [
+    'site_key' => env('TURNSTILE_SITE_KEY'),
+    'secret'   => env('TURNSTILE_SECRET_KEY'),
+],
 ];
